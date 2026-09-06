3843283fd4913a5abfa069f9289671ed7458735e
Divya Manku <divyaamarnadh2003@gmail.comgit config --global user.email divyaamarnadh2003@gmail.comgit config --global user.email divyaamarnadh2003@gmail.com>
2026-09-06 16:04:34 +0530

    DOM Manipulation

---
+++ index.js
@@ -0,0 +1,57 @@
+// let h1 = document.getElementById("h1")
+// let h1 = document.querySelector("h1")
+// let h1 = document.querySelector(".h1")
+// let h1 = document.querySelector("#h1")
+// let h1 = document.querySelectorAll("#h1")
+// console.log(h1);
+// console.log(h1, []);
+// console.log(h1.pop());
+
+// let p = document.querySelector("p")
+let p = document.querySelector("#desc")
+// p.textContent = "Hello Dosto"
+// p.textContent = "<h2> Hello Dostoooo </h2>" 
+// p.innerHTML = "<b> Hello Dostoooo </b1>" 
+// console.log(p.textContent);
+
+
+
+// p.setAttribute("nishant" , "saini")
+// p.setAttribute("style" , "background-color: pink; font-size : 50px")
+
+// let btn = document.querySelector("#btn")
+
+// btn.setAttribute("disabled", "true")
+// btn.textContent = "Remove"
+
+// let res = p.getAttribute("style")
+
+// console.log(res);
+
+// p.removeAttribute("style")
+
+
+// p.classList.add("random")
+// p.classList.remove("random")
+// p.classList.toggle("random")
+
+// console.log(p.classList.contains ("random"));
+// p.style.backgroundColor = "red"  //camel case
+
+// p.dataset.helloDostoHii = "hii"
+// console.log(p.dataset.helloDostoHii);
+// let div = document.createElement("div")
+// div.textContent = "Hello"
+// div2.textContent ="Div 2"
+
+// let body = document.querySelector("body")
+// console.log(body);
+
+// body.appendChild(div)
+// body.appendChild(div2)
+// body.append(div, div2)
+// body.prepend(div, div2)
+
+// console.log(body);
+
+
